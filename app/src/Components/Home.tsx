d9acc5849911447c135dfdfa7058442c4ffed35c
Raul Benitti <raul.benitti@southworks.com>
2021-03-23 16:59:56 -0300

    Update Home component
    Update Home

--- app/src/Components/Home.tsx
+++ app/src/Components/Home.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const Home = () => <div>
     <h1>Home</h1>
-    <p>This is the home page.</p>
+    <p>This is the home!</p>
     <p>Have a nice day :)</p>
 </div>
