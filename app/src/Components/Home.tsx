7eae10920248b3eb7290cbfa10e1b982580323ab
Raul Benitti <raul.benitti@southworks.com>
2021-03-25 18:29:09 -0300

    Update home component

--- app/src/Components/Home.tsx
+++ app/src/Components/Home.tsx
@@ -4,4 +4,5 @@ export const Home = () => <div>
     <h1>Home</h1>
     <p>This is the home page.</p>
     <p>Have a nice day also today :)</p>
+    <p>PUBLIC_URL: {process.env.PUBLIC_URL}</p>
 </div>
