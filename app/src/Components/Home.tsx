9375222b7a246dc978c94a2d204c0d827e5dd299
Raul Benitti <raul.benitti@southworks.com>
2021-03-25 09:49:32 -0300

    Update Home

--- app/src/Components/Home.tsx
+++ app/src/Components/Home.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 export const Home = () => <div>
     <h1>Home</h1>
     <p>This is the home page.</p>
-    <p>Have a nice day today :)</p>
+    <p>Have a nice day also today :)</p>
 </div>
