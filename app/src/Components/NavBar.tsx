be122dce82fa80b4ab9fee6b52fcc46f1e8381f5
Javier Langer <javier.langer@southworks.com>
2021-03-30 12:34:11 -0300

    first upload bubbles

--- app/src/Components/NavBar.tsx
+++ app/src/Components/NavBar.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export const NavBar = () => <div>
-    <ul>
+    <ul style={{ listStyle: 'none', display: 'flex', flexDirection: 'row', gap: '20px' }}>
         <li>
             <NavLink to="/">Home</NavLink>
         </li>
         <li>
             <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+            <NavLink to="/bubbles">Bubbles</NavLink>
         </li>
         <li>
             <NavLink to="/non-existant">404</NavLink>
